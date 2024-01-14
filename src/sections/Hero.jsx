f414e2a4b5d06627ea96b68d70825557cfddab4e
MerakiShubh <shubhamt.champ@gmail.com>
2024-01-14 23:49:21 +0530

    Changes in Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,9 @@
 const Hero = () => {
   return (
-    <section id="home" className="w-full border-2 border-red-500"></section>
+    <section
+      id="home"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container border-2 border-red-500"
+    ></section>
   );
 };
 
